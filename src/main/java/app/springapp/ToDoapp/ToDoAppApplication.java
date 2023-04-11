d2a551e9145c4c49d39bc9d3902586db95184b6e
Artur Sady <asady502@gmail.com>
2023-04-11 17:35:47 +0200

    Initial commit

---
+++ src/main/java/app/springapp/ToDoapp/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package app.springapp.ToDoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
